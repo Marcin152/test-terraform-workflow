edf57709e43890c594d904e64510d1cf1c52236a
Marcin Szymczyk <91828986+Marcin152@users.noreply.github.com>
2022-05-09 19:11:37 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ resource "local_file" "foo" {
     content  = "foo!"
     filename = "${path.module}/foo.bar"
 }
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+}
